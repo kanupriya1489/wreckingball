c79b75a4d9afdf2a5ceb47bd49164e84009295ee
kanupriya1489 <72565479+kanupriya1489@users.noreply.github.com>
2020-11-27 20:03:34 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,28 @@
+class Box{
+  constructor(x,y,width,height){
+    var options ={
+      'restitution' :0.9,
+      'friction':0.8,
+      'density':0.05
+    }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width  = width;
+    this.height = height;
+
+    World.add(world,this.body);
+  }
+  display(){
+  var pos =this.body.position; 
+  var angle = this.body.angle;
+  push() ;
+  translate(pos.x , pos.y);
+  rotate(angle);
+  rectMode(CENTER);
+  strokeWeight(5);
+  stroke("blue");
+  fill("yellow");
+  rect(0,0,this.width , this.height);
+  pop();
+
+  }
+}
